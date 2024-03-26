26f5dfa35ea90923087d9f32d520088c3bb5288a
cindy917 <90430653+weihsinyeh@users.noreply.github.com>
2024-03-27 02:59:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -42,13 +42,12 @@ window.addEventListener("load", async function() {
         video_arr = parent.document.querySelectorAll("img")
         //fill the content of video, title, and description      
         new_child.children[0].innerHTML += folder_name[i]; //title 
-
+        await loadJson(new_child.children[1], json_path);
         epoch = 80; 
         for (let j=0; j<5;j++){
           video_path = "./epoch"+(epoch+j).toString()+"/"+folder_name[i] + ".gif";
           video_arr[j].src = video_path;
         }
-        await loadJson(new_child.children[1], json_path);
         prediction_arr = parent.document.querySelectorAll("p")
         for (let j=0; j<5;j++){
             epoch_path = result_epoch + "/results_epoch" + (epoch+j).toString() + ".json"; 
